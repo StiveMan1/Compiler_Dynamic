bdf7fe60aea0be2ccf30af471b91ee1d54f40118
vladimirKa002 <kvvgames18@gmail.com>
2023-05-08 22:44:10 +0300

    Broken tests added

--- dynamic_test.c
+++ dynamic_test.c
@@ -1,6 +1,5 @@
 #include <dirent.h>
 #include <stdio.h>
-#include <unistd.h>
 #include "struct.h"
 #include "lexical_analysis.h"
 #include "ast_semantic.h"
@@ -17,18 +16,18 @@ char* concat(char* s1, char* s2){
 }
 
 int type_to_step(char* type){
-    if (strcmp(type, LEXICAL_ANALYSIS_ERROR) != 0) return 1;
-    else if (strcmp(type, SYNTAX_ANALYSIS_ERROR) != 0) return 3;
-    else if (strcmp(type, SEMANTIC_ANALYSIS_ERROR) != 0) return 2;
-    return 1;
+    if (strcmp(type, LEXICAL_ANALYSIS_ERROR) == 0) return 1;
+    else if (strcmp(type, SYNTAX_ANALYSIS_ERROR) == 0) return 2;
+    else if (strcmp(type, SEMANTIC_ANALYSIS_ERROR) == 0) return 3;
+    return 4;
 }
 
 void check_error(struct error_st *error, struct error_st *expected_error, char* type, struct la_parser* F_parser){
     int is_error_null = error == NULL || error_is_null(error);
     if (!is_error_null) {
-        print_error_log(error, F_parser);
+        print_error(error);
     }
-    if (expected_error == NULL){
+    if (error_is_null(expected_error)){
         if (is_error_null) return;
         else exit(1);
     }
@@ -40,22 +39,41 @@ void check_error(struct error_st *error, struct error_st *expected_error, char* 
     exit(1);
 }
 
-void read_error( char* error_file, struct error_st *expected_error){
-    struct la_parser *error_parser = la_parser_new();
-    struct token_st *token = NULL;
-    la_parser_set_file(error_parser, error_file);
-    tokenize(error_parser);
-    expected_error = error_new();
-    token = error_parser->list->data[2]->data;
-    string_set_str(expected_error->type, token->data, token->size);
-    token = error_parser->list->data[5]->data;
-    string_set_str(expected_error->message, token->data, token->size);
-    token = error_parser->list->data[8]->data;
-    expected_error->pos = strtol(token->data, NULL, 10);
-    token = error_parser->list->data[11]->data;
-    expected_error->line_num = strtol(token->data, NULL, 10);
-    token = error_parser->list->data[14]->data;
-    expected_error->line_pos = strtol(token->data, NULL, 10);
+void read_error(char* error_file, struct error_st *expected_error){
+    FILE* filePointer;
+    int bufferLength = 255;
+    char buffer[bufferLength];
+
+    filePointer = fopen(error_file, "r");
+
+    // Error type
+    fgets(buffer, bufferLength, filePointer);
+    string_set_str(expected_error->type, buffer, strlen(buffer) - 1);
+
+    // Error message
+    struct string_st* temp_message = string_new();
+    fgets(buffer, bufferLength, filePointer);
+    string_set_str(temp_message, buffer, strlen(buffer) - 1);
+    struct string_st* null_str = string_new();
+    string_set_str(null_str, "(null)", 6);
+    if (string_cmp(null_str, temp_message) != 0)
+        expected_error->message = temp_message;
+
+    // Error Pos
+    fgets(buffer, bufferLength, filePointer);
+    expected_error->pos = strtol(buffer, NULL, 10);
+
+    // Error Line Num
+    fgets(buffer, bufferLength, filePointer);
+    expected_error->line_num = strtol(buffer, NULL, 10);
+
+    // Error Line Pos
+    fgets(buffer, bufferLength, filePointer);
+    expected_error->line_pos = strtol(buffer, NULL, 10);
+
+    fclose(filePointer);
+
+    expected_error->present = 1;
 }
 
 void read_output(char* result_file, struct string_st *interp_out){
@@ -90,7 +108,7 @@ int main(int argc, char *argv[]) {
     char* result_file = concat(path, "/out.txt");
 
     struct error_st *error = NULL;
-    struct error_st *expected_error = NULL;
+    struct error_st *expected_error = error_new();
     struct string_st *expected_interp_out = string_new();
 
     struct la_parser *F_parser = la_parser_new();
@@ -106,12 +124,14 @@ int main(int argc, char *argv[]) {
 
     // Lexical
     tokenize(F_parser);
+    error = F_parser->error_obj;
     check_error(error, expected_error, LEXICAL_ANALYSIS_ERROR, F_parser);
 
     // AST parser
     object_set_type(expr_obj, NODE_TYPE);
     ast_parser_set_list(T_parser, F_parser);
     token_analyzer(T_parser, expr_obj->data);
+    error = T_parser->error_obj;
     check_error(error, expected_error, SYNTAX_ANALYSIS_ERROR, F_parser);
 
     error = error_new();
@@ -134,9 +154,6 @@ int main(int argc, char *argv[]) {
     close(fd[1]);
     dup2(stdout_bk, fileno(stdout));//restore
 
-    ast_parser_free(T_parser);
-    la_parser_free(F_parser);
-
     char ch;
     int index = 0;
     while(read(fd[0], &ch, 1) > 0) {
@@ -145,4 +162,7 @@ int main(int argc, char *argv[]) {
         index++;
     }
     if (expected_interp_out->size != index) exit(1);
+
+    ast_parser_free(T_parser);
+    la_parser_free(F_parser);
 }
